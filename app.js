58062663dc63d099b6def32e5e9864845b767ef4
Hasret Ünal <s.hasretunal@gmail.com>
2022-07-31 18:17:01 +0300

    project completed

---
+++ app.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const getUser = (userId) => {
+    return new Promise (async (resolve, reject) => {
+        const {data} = await axios("https://jsonplaceholder.typicode.com/users/" + userId);
+        resolve(data);
+    });
+};
+
+const getPost = (userId) => {
+    return new Promise (async (resolve, reject) => {
+        const {data} = await axios("https://jsonplaceholder.typicode.com/posts?userId=" + userId);
+        resolve(data);
+    });
+};
+
+async function getData(userId){
+    try {
+        const user = await getUser(userId);
+        const posts = await getPost(userId);
+
+        user.posts = posts;
+        console.log(user)
+    }
+    catch(e){
+        console.log(e);
+    }
+}
+export default getData;
---
+++ index.js
@@ -0,0 +1,5 @@
+import getData from "./app.js";
+
+getData(1)
+    .then(data => console.log(data))
+    .catch(e => console.log(e));
